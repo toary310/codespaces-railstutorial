9cb24542688de0f5573e9ee7dfaa25398dd24b38
toary310 <ryota.sato@andpad.co.jp>
2024-02-06 17:18:56 +0900

    change route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
     def hello
         render html: 'Hello world!'
     end
+
+    def goodbye
+        render html: 'Goodbye world!'
+    end
 end
